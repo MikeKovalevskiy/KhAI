ccb9f710d5b9870800124747a1c34a8fa22430d1
Kukold98 <kukold@mail.ru>
2020-02-12 12:49:03 +0200

    Added char variable. And changed other.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@ int main(int argc, char *argv[ ])
 {
     QApplication app(argc, argv);
  	
-    int a = 5;
-	double b = 1.0;
+    int i = 1;
+	double d = 1.0;
+	char c = 'c';
     
 	//how to make conflict?!
     QLabel *label = new QLabel("<h2 style='color: green'>Hello, World! Life is great!</h2>");
