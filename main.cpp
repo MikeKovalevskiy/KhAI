715eb0008884e02e80317cc7cf37002e2c84b707
Kukold98 <kukold@mail.ru>
2020-02-12 12:47:10 +0200

    Added double variable.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[ ])
     QApplication app(argc, argv);
  	
     int a = 5;
+	double b = 1.0;
     
 	//how to make conflict?!
     QLabel *label = new QLabel("<h2 style='color: green'>Hello, World! Life is great!</h2>");
